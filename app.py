2d1465ad2038525c69cf5405b0932a9669c3648d
Melanye <alarconmelanye@gmail.com>
2024-03-16 20:04:57 -0500

    commit message

--- app.py
+++ app.py
@@ -30,3 +30,4 @@ build_histogram = st.checkbox('Construir un histograma')
 
 if build_histogram: # si la casilla de verificación está seleccionada
     st.write('Construir un histograma para la columna odómetro')
+    fig = px.histogram(car_data, x="odometer")
