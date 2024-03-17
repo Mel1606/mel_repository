083125bf193469afbf1ad9944cf6e3a9db135813
Melanye <alarconmelanye@gmail.com>
2024-03-16 20:21:00 -0500

    commit message

--- app.py
+++ app.py
@@ -24,11 +24,3 @@ if dis_button: # al hacer clic en el botón
         
 # mostrar un gráfico Plotly interactivo
     st.plotly_chart(dis, use_container_width=True)
-    
-# crear una casilla de verificación
-build_histogram = st.checkbox('Construir un histograma')
-
-if build_histogram: # si la casilla de verificación está seleccionada
-    st.write('Construir un histograma para la columna odómetro')
-    fig = px.histogram(car_data, x="odometer")
-    st.plotly_chart(fig, use_container_width=True)
